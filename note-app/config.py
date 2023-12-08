50c71810c2c007658b6172b14bd37c230d7e4eea
AsukaTan <qq3536537266@gmail.com>
2023-12-08 09:36:50 -0500

    include jequry build file. So do not worrry about environment for npm

--- note-app/config.py
+++ note-app/config.py
@@ -8,14 +8,15 @@ class Config(object):
 class DevelopmentConfig(Config):
     """开发环境配置"""
     DEBUG = True
-    MONGO_URI = 'mongodb://localhost:27017/Development'
+    MONGO_URI = 'mongodb://db:27017/Development'
 
 
 class TestingConfig(Config):
     """测试环境配置"""
     TESTING = True
-    MONGO_URI = 'mongodb://localhost:27017/Test'
+    MONGO_URI = 'mongodb://db:27017/Test'
+
 
 class ProductionConfig(Config):
     """生产环境配置"""
-    MONGO_URI = 'your_production_mongo_uri'
+    MONGO_URI = 'mongodb://db:27017/Production'
