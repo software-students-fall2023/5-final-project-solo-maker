a81bb9b890fa4f447719c3b9dac1f2e9085e5d57
AsukaTan <89984302+AsukaTan@users.noreply.github.com>
2023-12-18 17:51:33 -0500

    Update app.py

--- note-app/app.py
+++ note-app/app.py
@@ -1,7 +1,7 @@
 from src import create_app
-from config import ProductionConfig
+from config import TestingConfig
 
-app = create_app(ProductionConfig)
+app = create_app(TestingConfig)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True, host='0.0.0.0')
