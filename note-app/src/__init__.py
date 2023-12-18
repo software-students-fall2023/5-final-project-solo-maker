f73ab86fcfb0f3e9c418b8023d1de6b6a2075dab
AsukaTan <89984302+AsukaTan@users.noreply.github.com>
2023-12-18 16:27:41 -0500

    Update __init__.py

--- note-app/src/__init__.py
+++ note-app/src/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 mongo = PyMongo()
 
 
 def create_app(config_class):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_class)
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
